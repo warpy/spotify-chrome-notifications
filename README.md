34171ff6037ff3f0494b3e76885ac1b1def73ce6
Sanket Parab <sanketsp@gmail.com>
2014-02-28 22:50:38 -0800

    no longer keeping a single struct for notifications

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ spotify-chrome-notifications
 ============================
 
 A Google Chrome extension that notifies you every time your Spotify Web Player plays a new song.
+
+###Installation
---
+++ notifications.js
@@ -0,0 +1,49 @@
+if (window.webkitNotifications) {
+  var stay = 4000,
+    interval = 1000,
+    hash = hex_md5("temp"),
+    checks = {
+      art: function() {
+        var $img = $('#cover-art').find('.sp-image-img');
+        if ($img.length > 0) {
+          return $img.css('background-image').replace("url(", "").replace(")", "");
+        }
+        return null;
+      },
+      name: function() {
+        return $('#track-name').text();
+      },
+      artist: function() {
+        return $('#track-artist').text();
+      }
+    };
+  if (!localStorage.scn_hash) {
+    localStorage.scn_hash = hash;
+  }
+  setInterval(function() {
+    var results = {}, hash = null,
+      text = null,
+      cont = true;
+    for (var i in checks) {
+      if (checks.hasOwnProperty(i)) {
+        text = checks[i].call();
+        if (typeof text != "undefined" && text != null && text.length > 0) {
+          results[i] = text;
+        } else {
+          cont = false;
+        }
+      }
+    }
+    if (cont) {
+      hash = hex_md5(JSON.stringify(results));
+      if (localStorage.scn_hash !== hash) {
+        localStorage.scn_hash = hash;
+        var notification = window.webkitNotifications.createNotification(results.art, results.name, results.artist);
+        notification.show();
+        setTimeout(function() {
+          notification.cancel();
+        }, stay);
+      }
+    }
+  }, interval);
+}

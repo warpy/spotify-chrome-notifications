82fa246c8b5e9583e6134d4952371b2b92381fe4
Sanket Parab <sanketsp@gmail.com>
2014-12-08 14:31:52 -0800

    updated the source with the new Notification API

--- notifications.js
+++ notifications.js
@@ -1,51 +1,59 @@
-console.log(chrome.notifications);
-
-if (window.webkitNotifications) {
-  var stay = 4000,
-    interval = 1000,
-    hash = hex_md5("temp"),
-    checks = {
-      art: function() {
-        var $img = $('#cover-art').find('.sp-image-img');
-        if ($img.length > 0) {
-          return $img.css('background-image').replace("url(", "").replace(")", "");
-        }
-        return null;
-      },
-      name: function() {
-        return $('#track-name').text();
-      },
-      artist: function() {
-        return $('#track-artist').text();
-      }
-    };
-  if (!localStorage.scn_hash) {
-    localStorage.scn_hash = hash;
+var alert_str = "[Spotify Chrome Notification]";
+console.log(alert_str, "initializing (available: " + (typeof window.Notification != 'undefined') + ")");
+var stay = 4000;
+var interval = 1000;
+var hash = hex_md5("temp");
+var checks = {
+  art: function() {
+    var $img = $('#cover-art').find('.sp-image-img');
+    if ($img.length > 0) {
+      return $img.css('background-image').replace("url(", "").replace(")", "");
+    }
+    return null;
+  },
+  name: function() {
+    return $('#track-name').text();
+  },
+  artist: function() {
+    return $('#track-artist').text();
   }
-  setInterval(function() {
-    var results = {}, hash = null,
-      text = null,
-      cont = true;
-    for (var i in checks) {
-      if (checks.hasOwnProperty(i)) {
-        text = checks[i].call();
-        if (typeof text != "undefined" && text != null && text.length > 0) {
-          results[i] = text;
-        } else {
-          cont = false;
+};
+if (window.Notification) {
+  console.log(alert_str, "requesting permission");
+  window.Notification.requestPermission(function() {
+    console.log(alert_str, "permission granted");
+    if (!localStorage.scn_hash) {
+      localStorage.scn_hash = hash;
+    }
+    setInterval(function() {
+      var result = {};
+      var hash = null;
+      var text = null;
+      var cont = true;
+      for (var i in checks) {
+        if (checks.hasOwnProperty(i)) {
+          text = checks[i].call();
+          if (typeof text != "undefined" && text != null && text.length > 0) {
+            result[i] = text;
+          } else {
+            cont = false;
+          }
         }
       }
-    }
-    if (cont) {
-      hash = hex_md5(JSON.stringify(results));
-      if (localStorage.scn_hash !== hash) {
-        localStorage.scn_hash = hash;
-        var notification = window.webkitNotifications.createNotification(results.art, results.name, results.artist);
-        notification.show();
-        setTimeout(function() {
-          notification.cancel();
-        }, stay);
+      if (cont) {
+        hash = hex_md5(JSON.stringify(result));
+        if (localStorage.scn_hash !== hash) {
+          localStorage.scn_hash = hash;
+          console.log(alert_str, "new song", result);
+          var notification = new window.Notification(result.name, {
+            body: result.artist,
+            icon: result.art
+          });
+          setTimeout(function() {
+            notification.close();
+          }, stay);
+        }
       }
-    }
-  }, interval);
+    }, interval);
+  });
 }
